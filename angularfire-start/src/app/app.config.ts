9c4f1cbe8f4377a7bd38f115c14ee9571cff83a2
Murali <muralisudireddy0@gmail.com>
2025-05-18 00:24:43 +0530

    message feature added

--- angularfire-start/src/app/app.config.ts
+++ angularfire-start/src/app/app.config.ts
@@ -11,14 +11,14 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => {
+      return getMessaging();
+  }),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"codelab-friendlychat-web-1f303","appId":"1:125086314336:web:434c7b30741e272ca07805","storageBucket":"codelab-friendlychat-web-1f303.firebasestorage.app","apiKey":"AIzaSyAxPDAghKEJwCmaZa9Se1yZUjX410LZ28g","authDomain":"codelab-friendlychat-web-1f303.firebaseapp.com","messagingSenderId":"125086314336","measurementId":"G-N1DDK63CYL"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
--- angularfire-start/src/environments/environment.ts
+++ angularfire-start/src/environments/environment.ts
@@ -1,12 +1,21 @@
 export const environment = {
+  production: false,
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyAxPDAghKEJwCmaZa9Se1yZUjX410LZ28g",
+    authDomain: "codelab-friendlychat-web-1f303.firebaseapp.com",
+    projectId: "codelab-friendlychat-web-1f303",
+    storageBucket: "codelab-friendlychat-web-1f303.firebasestorage.app",
+    messagingSenderId: "125086314336",
+    appId: "1:125086314336:web:434c7b30741e272ca07805",
+    measurementId: "G-N1DDK63CYL",
   },
+  googleOAuth: {
+    client_id: "125086314336-j2pr87hbm89hhgvbj2eag8rk0r1m2r9u.apps.googleusercontent.com",
+    client_secret: "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",
+    redirect_uris: [
+      "http://localhost:4200",
+      "https://codelab-friendlychat-web-1f303.web.app"
+    ],
+    javascript_origins: ["http://localhost:4200"]
+  }
 };
